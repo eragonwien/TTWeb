22af65928d9215b1a73a2e870d4eca53ca1cf460
Hoang Son Nguyen <eragonwien@gmail.com>
2019-11-24 03:39:37 +0100

    -- creates basic MVC project
    -- creates basic facebook authentication

---
+++ TTWebMVC/TTWebMVC/Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Facebook;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using TTWebMVC.Models.Common;
+
+namespace TTWebMVC.Controllers
+{
+   [AllowAnonymous]
+   public class AccountController : Controller
+   {
+      public IActionResult Login()
+      {
+         return View();
+      }
+
+      public IActionResult LoginExternal()
+      {
+         var authProperties = new AuthenticationProperties { RedirectUri = Url.Action(nameof(LoginCallback)) };
+         return Challenge(authProperties, FacebookDefaults.AuthenticationScheme);
+      }
+
+      public async Task<IActionResult> LoginCallback()
+      {
+         var authResult = await HttpContext.AuthenticateAsync(AuthenticationSettings.SchemeExternal);
+
+         return RedirectToAction("index", "home");
+      }
+   }
+}
---
+++ TTWebMVC/TTWebMVC/Models/Common/Settings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TTWebMVC.Models.Common
+{
+   public class Settings
+   {
+
+   }
+
+   public class AuthenticationSettings
+   {
+      public const string SchemeApplication = "Application";
+      public const string SchemeExternal = "External";
+   }
+}
