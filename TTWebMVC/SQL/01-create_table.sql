6dd589a4b074e2f8d61598682f08bbcdf2b3f925
Hoang Son Nguyen <eragonwien@gmail.com>
2019-11-29 00:04:01 +0100

    changes to naming of id

--- TTWebMVC/SQL/01-create_table.sql
+++ TTWebMVC/SQL/01-create_table.sql
@@ -3,19 +3,19 @@ use ttweb;
 drop table if exists UserGroupMapping, UserGroup, Partner, AppUser;
 
 create table AppUser (
-	appuser_id int auto_increment primary key,
+	id int auto_increment primary key,
     email varchar(64) unique not null,
     firstname varchar(64),
     lastname varchar(64),
     facebook_id varchar(64),
 	access_token varchar(1024),
-    access_token_expired_at date,
+    access_token_expiration_date date,
     create_date date not null,
     update_date date
 );
 
 create table Partner (
-	partner_id int auto_increment primary key,
+	id int auto_increment primary key,
     email varchar(64) unique not null,
     firstname varchar(64),
     lastname varchar(64),
@@ -25,22 +25,22 @@ create table Partner (
 );
 
 create table UserGroup (
-	usergroup_id int auto_increment primary key,
+	id int auto_increment primary key,
     appuser_id int not null,
     title varchar(16),
     create_date date not null,
     update_date date,
-    foreign key (appuser_id) references AppUser(appuser_id)
+    foreign key (appuser_id) references AppUser(id)
 );
 
 create table UserGroupMapping (
-	usergroupmapping_id int auto_increment primary key,
+	id int auto_increment primary key,
 	usergroup_id int not null,
     partner_id int not null,
     create_date date not null,
     update_date date,
-	foreign key (usergroup_id) references UserGroup(usergroup_id),
-    foreign key (partner_id) references Partner(partner_id),
+	foreign key (usergroup_id) references UserGroup(id),
+    foreign key (partner_id) references Partner(id),
     constraint Unique_usergroup_partner unique (usergroup_id, partner_id)
 );
 
