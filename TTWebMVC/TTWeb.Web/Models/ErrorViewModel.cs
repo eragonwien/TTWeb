2c0fb125bbc113b804abc17cb3b2616bf4cb8038
Hoang Son Nguyen <eragonwien@gmail.com>
2020-10-03 22:03:42 +0200

    removes unused projects

---
+++ TTWebMVC/TTWeb.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TTWeb.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
