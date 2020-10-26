856d999a596214d044b1e31f74c0f00bd4cce73e
Hoang Son Nguyen <eragonwien@gmail.com>
2020-10-26 03:06:59 +0100

    adds migration for previous model changes

---
+++ TTWebMVC/TTWeb.Data/Migrations/20201026020628_AddSchedulingEntities.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TTWeb.Data.Migrations
+{
+    public partial class AddSchedulingEntities : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "LoginUsers",
+                maxLength: 64,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(64) CHARACTER SET utf8mb4",
+                oldMaxLength: 64,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "LoginUsers",
+                maxLength: 64,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(64) CHARACTER SET utf8mb4",
+                oldMaxLength: 64,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "LoginUsers",
+                maxLength: 64,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(64) CHARACTER SET utf8mb4",
+                oldMaxLength: 64,
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "FacebookUser",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Username = table.Column<string>(maxLength: 256, nullable: false),
+                    Password = table.Column<string>(maxLength: 256, nullable: false),
+                    SendScheduleId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FacebookUser", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Weekday",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Value = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Weekday", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Schedule",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Action = table.Column<string>(nullable: false),
+                    IntervalType = table.Column<string>(nullable: false),
+                    SenderId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Schedule", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Schedule_FacebookUser_SenderId",
+                        column: x => x.SenderId,
+                        principalTable: "FacebookUser",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ScheduleJob",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    ScheduleId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ScheduleJob", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ScheduleJob_Schedule_ScheduleId",
+                        column: x => x.ScheduleId,
+                        principalTable: "Schedule",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ScheduleReceiverMapping",
+                columns: table => new
+                {
+                    ScheduleId = table.Column<int>(nullable: false),
+                    ReceiverId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ScheduleReceiverMapping", x => new { x.ScheduleId, x.ReceiverId });
+                    table.ForeignKey(
+                        name: "FK_ScheduleReceiverMapping_FacebookUser_ReceiverId",
+                        column: x => x.ReceiverId,
+                        principalTable: "FacebookUser",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ScheduleReceiverMapping_Schedule_ScheduleId",
+                        column: x => x.ScheduleId,
+                        principalTable: "Schedule",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ScheduleWeekdayMapping",
+                columns: table => new
+                {
+                    ScheduleId = table.Column<int>(nullable: false),
+                    WeekdayId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ScheduleWeekdayMapping", x => new { x.ScheduleId, x.WeekdayId });
+                    table.ForeignKey(
+                        name: "FK_ScheduleWeekdayMapping_Schedule_ScheduleId",
+                        column: x => x.ScheduleId,
+                        principalTable: "Schedule",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ScheduleWeekdayMapping_Weekday_WeekdayId",
+                        column: x => x.WeekdayId,
+                        principalTable: "Weekday",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TimeFrame",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    ScheduleId = table.Column<int>(nullable: false),
+                    From = table.Column<DateTimeOffset>(nullable: false),
+                    To = table.Column<DateTimeOffset>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TimeFrame", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TimeFrame_Schedule_ScheduleId",
+                        column: x => x.ScheduleId,
+                        principalTable: "Schedule",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ScheduleJobResult",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    ScheduleJobId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ScheduleJobResult", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ScheduleJobResult_ScheduleJob_ScheduleJobId",
+                        column: x => x.ScheduleJobId,
+                        principalTable: "ScheduleJob",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FacebookUser_Username",
+                table: "FacebookUser",
+                column: "Username",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Schedule_SenderId",
+                table: "Schedule",
+                column: "SenderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ScheduleJob_ScheduleId",
+                table: "ScheduleJob",
+                column: "ScheduleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ScheduleJobResult_ScheduleJobId",
+                table: "ScheduleJobResult",
+                column: "ScheduleJobId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ScheduleReceiverMapping_ReceiverId",
+                table: "ScheduleReceiverMapping",
+                column: "ReceiverId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ScheduleWeekdayMapping_WeekdayId",
+                table: "ScheduleWeekdayMapping",
+                column: "WeekdayId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TimeFrame_ScheduleId",
+                table: "TimeFrame",
+                column: "ScheduleId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ScheduleJobResult");
+
+            migrationBuilder.DropTable(
+                name: "ScheduleReceiverMapping");
+
+            migrationBuilder.DropTable(
+                name: "ScheduleWeekdayMapping");
+
+            migrationBuilder.DropTable(
+                name: "TimeFrame");
+
+            migrationBuilder.DropTable(
+                name: "ScheduleJob");
+
+            migrationBuilder.DropTable(
+                name: "Weekday");
+
+            migrationBuilder.DropTable(
+                name: "Schedule");
+
+            migrationBuilder.DropTable(
+                name: "FacebookUser");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "LoginUsers",
+                type: "varchar(64) CHARACTER SET utf8mb4",
+                maxLength: 64,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 64);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "LoginUsers",
+                type: "varchar(64) CHARACTER SET utf8mb4",
+                maxLength: 64,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 64);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "LoginUsers",
+                type: "varchar(64) CHARACTER SET utf8mb4",
+                maxLength: 64,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 64);
+        }
+    }
+}
