7333448ebc46f77df8a4c860990a95c96eba9b98
Hoang Son Nguyen <eragonwien@gmail.com>
2020-10-30 01:06:22 +0100

    adds global exception handler middleware

---
+++ TTWebMVC/TTWeb.Web.Api/Middlewares/WebApiExceptionHandlerMiddleware.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace TTWeb.Web.Api.Middlewares
+{
+    public class WebApiExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger _logger;
+
+        public WebApiExceptionHandlerMiddleware(RequestDelegate next, ILogger logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Unhandled exception occurred");
+                await HandleExceptionAsync(httpContext, ex);
+            }
+        }
+
+        private Task HandleExceptionAsync(HttpContext httpContext, Exception ex)
+        {
+            httpContext.Response.ContentType = "application/json";
+            httpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            return httpContext.Response.WriteAsync(new
+            {
+                StatusCode = httpContext.Response.StatusCode,
+                Message = "Internal Server Error"
+            }.ToString());
+        }
+    }
+}
