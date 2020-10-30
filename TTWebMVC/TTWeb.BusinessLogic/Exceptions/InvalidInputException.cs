8d7c79d32650210811f16902ca6e881d00e69022
Hoang Son Nguyen <h.nguyen@greenbird.fm>
2020-10-30 12:43:40 +0100

    adds new exceptions for web api

---
+++ TTWebMVC/TTWeb.BusinessLogic/Exceptions/InvalidInputException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TTWeb.BusinessLogic.Exceptions
+{
+    [Serializable]
+    public class InvalidInputException : Exception
+    {
+        public InvalidInputException()
+        {
+
+        }
+
+        public InvalidInputException(string message)
+            : base(message)
+        {
+
+        }
+
+        public InvalidInputException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+
+        }
+    }
+}
---
+++ TTWebMVC/TTWeb.BusinessLogic/Exceptions/ResourceNotFoundException.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace TTWeb.BusinessLogic.Exceptions
+{
+    [Serializable]
+    public class ResourceNotFoundException<T> : Exception
+    {
+        public ResourceNotFoundException()
+        {
+
+        }
+
+        public ResourceNotFoundException(string message)
+            : base(message)
+        {
+
+        }
+
+        public ResourceNotFoundException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+
+        }
+
+        public ResourceNotFoundException(T resource) : base(BuildMessage(resource))
+        {
+        }
+
+        private static string BuildMessage(T resource)
+        {
+            return $"Resource '{nameof(resource)}' not found";
+        }
+    }
+}
