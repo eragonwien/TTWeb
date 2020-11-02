bce73ec60e907253664194e3c8f341086f59accb
Hoang Son Nguyen <h.nguyen@greenbird.fm>
2020-11-02 10:17:39 +0100

    adds facebook user controller + TODOs

---
+++ TTWebMVC/TTWeb.Web.Api/Controllers/FacebookUserController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TTWeb.Web.Api.Controllers
+{
+    [Route("api/facebook-users")]
+    [ApiController]
+    public class FacebookUserController : BaseController
+    {
+        // TODO: encrypts password of facebook user in authentication service
+        // TODO: adds facebook user of login user
+        // TODO: gets facebook users of login user
+        // TODO: updates facebook user of login user
+        // TODO: deletes facebook user of login user
+    }
+}
