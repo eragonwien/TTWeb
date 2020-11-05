535ae0d9df75ea30ea339b8b68fb36d0137aeeff
Hoang Son Nguyen <h.nguyen@greenbird.fm>
2020-11-05 12:14:59 +0100

    adds empty schedule service

---
+++ TTWebMVC/TTWeb.BusinessLogic/Services/Schedule/IScheduleService.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using TTWeb.BusinessLogic.Models.Entities;
+
+namespace TTWeb.BusinessLogic.Services.Schedule
+{
+    public interface IScheduleService
+    {
+        Task<ScheduleModel> CreateAsync(ScheduleModel model);
+        Task<ScheduleModel> UpdateAsync(ScheduleModel model);
+        Task DeleteAsync(int id, int ownerId);
+        Task<ScheduleModel> ReadByIdAsync(int id, int ownerId);
+    }
+}
---
+++ TTWebMVC/TTWeb.BusinessLogic/Services/Schedule/ScheduleService.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using TTWeb.BusinessLogic.Models.Entities;
+
+namespace TTWeb.BusinessLogic.Services.Schedule
+{
+    public class ScheduleService : IScheduleService
+    {
+        public Task<ScheduleModel> CreateAsync(ScheduleModel model)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<ScheduleModel> UpdateAsync(ScheduleModel model)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task DeleteAsync(int id, int ownerId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<ScheduleModel> ReadByIdAsync(int id, int ownerId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
