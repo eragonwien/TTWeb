8951514ba6c376accb972e423ed28f2a95ef00d4
Hoang Son Nguyen <h.nguyen@greenbird.fm>
2020-11-09 13:15:51 +0100

    adds empty worker service for schedule calculation

---
+++ TTWebMVC/TTWeb.Worker.CalculateSchedule/Program.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace TTWeb.Worker.CalculateSchedule
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<Worker>();
+                })
+                .ConfigureLogging(o =>
+                {
+                    o.ClearProviders();
+                    o.AddConsole();
+                    o.SetMinimumLevel(LogLevel.Information);
+                });
+    }
+}
