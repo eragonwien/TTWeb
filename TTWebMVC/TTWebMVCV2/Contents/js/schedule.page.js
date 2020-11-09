8e28c0ea3510fb9e63b8eb184ba6f1487145c2b6
Hoang Son Nguyen <h.nguyen@greenbird.fm>
2020-11-09 13:58:15 +0100

    removes unused projects

--- TTWebMVC/TTWebMVCV2/Contents/js/schedule.page.js
+++
@@ -1,7 +0,0 @@
-﻿$(document).on('click', '.guess-timezone-button', function () {
-   var ianiaTz = Intl.DateTimeFormat().resolvedOptions().timeZone;
-   var selectlist = $($(this).attr('data-selectlist'));
-   ajaxPost($(this).attr('data-href'), { timeZone: ianiaTz }, function (timezone) {
-      selectlist.val(timezone)
-   });
-});
--- TTWebMVC/TTWebMVCV2/Models/ViewModels/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TTWebMVCV2.Models.ViewModels
-{
-   public class ErrorViewModel
-   {
-      public string RequestId { get; set; }
-
-      public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-   }
-}
