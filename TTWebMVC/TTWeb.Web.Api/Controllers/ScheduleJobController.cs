d091281fbcd73fc55e42434a87e92e0b2c7f8c05
Hoang Son Nguyen <h.nguyen@greenbird.fm>
2020-11-09 15:42:28 +0100

    adds empty controllers for schedule jobs & results

---
+++ TTWebMVC/TTWeb.Web.Api/Controllers/ScheduleJobController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TTWeb.Web.Api.Controllers
+{
+    [Route("api/schedule-jobs")]
+    [ApiController]
+    public class ScheduleJobController : BaseController
+    {
+    }
+}
---
+++ TTWebMVC/TTWeb.Web.Api/Controllers/ScheduleResultController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TTWeb.Web.Api.Controllers
+{
+    [Route("api/schedule-results")]
+    [ApiController]
+    public class ScheduleResultController : BaseController
+    {
+    }
+}
