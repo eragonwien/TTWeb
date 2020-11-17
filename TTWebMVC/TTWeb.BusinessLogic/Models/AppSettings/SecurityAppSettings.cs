218a49e22c17438c8ab88af5435bf9dcbfbd21d0
Hoang Son Nguyen <h.nguyen@greenbird.fm>
2020-11-17 09:23:07 +0100

    move class to separate files

--- TTWebMVC/TTWeb.BusinessLogic/Models/AppSettings/SecurityAppSettings.cs
+++ TTWebMVC/TTWeb.BusinessLogic/Models/AppSettings/SecurityAppSettings.cs
@@ -7,19 +7,4 @@
         public SecurityEncryptionAppSettings Encryption { get; set; }
         public SecurityCorsAppSettings Cors { get; set; }
     }
-
-    public class SecurityEncryptionAppSettings
-    {
-        public const string Section = "Encryption";
-
-        public string Key { get; set; }
-        public string Iv { get; set; }
-    }
-
-    public class SecurityCorsAppSettings
-    {
-        public const string Section = "Cors";
-
-        public string[] Origins { get; set; } = new string[0];
-    }
 }
