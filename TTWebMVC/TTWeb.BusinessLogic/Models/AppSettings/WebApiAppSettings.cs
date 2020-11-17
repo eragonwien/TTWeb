77b1489904cce66e2e4acd1c84b831bcc54959a5
Hoang Son Nguyen <nguyennhoangson@gmail.com>
2020-11-18 00:02:53 +0100

    updates models of appsettings.json

--- TTWebMVC/TTWeb.BusinessLogic/Models/AppSettings/WebApiAppSettings.cs
+++ TTWebMVC/TTWeb.BusinessLogic/Models/AppSettings/WebApiAppSettings.cs
@@ -1,10 +1,18 @@
-﻿namespace TTWeb.BusinessLogic.Models.AppSettings
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+
+namespace TTWeb.BusinessLogic.Models.AppSettings
 {
     public class WebApiAppSettings
     {
         public const string Section = "WebApi";
 
         public string BaseUri { get; set; }
-        public WebApiAppSettings Routes { get; set; }
+        public WebApiRoutesAppSettings Routes { get; set; }
+
+        public string GetRoute(string routePostFix)
+        {
+            return string.Join("/", BaseUri, routePostFix);
+        }
     }
 }
