e5de0e725d1e0b63e50a63281770090f99cd1323
Hoang Son Nguyen <nguyennhoangson@gmail.com>
2020-11-17 17:32:31 +0100

    removes default worker class

--- TTWebMVC/TTWeb.Worker.CalculateSchedule/Program.cs
+++ TTWebMVC/TTWeb.Worker.CalculateSchedule/Program.cs
@@ -17,7 +17,6 @@ namespace TTWeb.Worker.CalculateSchedule
                 .ConfigureServices((hostContext, services) =>
                 {
                     services.AddHostedService<Worker>();
-                    services.AddScoped<IScheduleService, ScheduleService>();
                 })
                 .ConfigureLogging(o =>
                 {
