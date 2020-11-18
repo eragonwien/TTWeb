f1a2584edad875718a316e5860dba62a274f1bf2
Hoang Son Nguyen <h.nguyen@greenbird.fm>
2020-11-18 09:06:41 +0100

    adds user agent to web api settings

--- TTWebMVC/TTWeb.BusinessLogic/Models/AppSettings/WebApiAppSettings.cs
+++ TTWebMVC/TTWeb.BusinessLogic/Models/AppSettings/WebApiAppSettings.cs
@@ -2,7 +2,10 @@
 {
     public class WebApiAppSettings
     {
+        public readonly string ClientName = nameof(HttpClientAppSettings.WebApi);
+
         public string BaseAddress { get; set; }
+        public string UserAgent { get; set; }
         public WebApiRoutesAppSettings Routes { get; set; }
 
         public string GetRoute(params string[] routeParts)
