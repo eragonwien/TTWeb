67dc022997ce1e6b0aeac37d94d6b6fb43414b98
Hoang Son Nguyen <nguyennhoangson@gmail.com>
2020-11-23 23:45:25 +0100

    hides internal properties of tokenmodel

--- TTWebMVC/TTWeb.BusinessLogic/Models/Account/TokenModel.cs
+++ TTWebMVC/TTWeb.BusinessLogic/Models/Account/TokenModel.cs
@@ -1,13 +1,18 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 
 namespace TTWeb.BusinessLogic.Models.Account
 {
     public class TokenModel
     {
         public string Token { get; set; }
+
         public DateTime ExpirationDateUtc { get; set; }
 
+        [JsonIgnore]
         public bool IsEmpty => string.IsNullOrWhiteSpace(Token);
+
+        [JsonIgnore]
         public bool Expired => ExpirationDateUtc < DateTime.UtcNow;
     }
 }
