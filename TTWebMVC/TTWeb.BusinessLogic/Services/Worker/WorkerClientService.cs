524f71698d36689bcb9191798c3c95672eb32c4f
Hoang Son Nguyen <nguyennhoangson@gmail.com>
2020-11-25 22:45:56 +0100

    moves configuration of worker service into one class

--- TTWebMVC/TTWeb.BusinessLogic/Services/Worker/WorkerClientService.cs
+++ TTWebMVC/TTWeb.BusinessLogic/Services/Worker/WorkerClientService.cs
@@ -1,8 +1,15 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using TTWeb.BusinessLogic.Models.AppSettings;
+using TTWeb.BusinessLogic.Models.AppSettings.Authentication;
+using TTWeb.BusinessLogic.Models.AppSettings.Scheduling;
 using TTWeb.BusinessLogic.Models.AppSettings.WebApi;
+using TTWeb.BusinessLogic.Services.Authentication;
 using TTWeb.BusinessLogic.Services.Client;
 
 namespace TTWeb.BusinessLogic.Services.Worker
@@ -25,6 +32,34 @@ namespace TTWeb.BusinessLogic.Services.Worker
             var response = await _webApiClient.PostAsync(_webApiAppSettings.Routes.TriggerPlanning);
             response.EnsureSuccessStatusCode();
             return Convert.ToInt32(await response.Content.ReadAsStringAsync());
+        }
+
+        public static void ConfigureAppConfiguration(HostBuilderContext context, IConfigurationBuilder configuration)
+        {
+            configuration
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile($"appsettings.json", optional: true)
+                .AddJsonFile($"appsettings.{context.HostingEnvironment.EnvironmentName}.json", optional: false);
+        }
+
+        public static void ConfigureServices(HostBuilderContext context, IServiceCollection services)
+        {
+            services.Configure<HttpClientAppSettings>(context.Configuration.GetSection(HttpClientAppSettings.Section));
+            services.Configure<WorkerAppSettings>(context.Configuration.GetSection(WorkerAppSettings.Section));
+            services.Configure<AuthenticationAppSettings>(context.Configuration.GetSection(AuthenticationAppSettings.Section));
+            services.Configure<SchedulingAppSettings>(context.Configuration.GetSection(SchedulingAppSettings.Section));
+
+            services.AddHttpClient<WorkerWebApiClient>();
+            services.AddSingleton<IAuthenticationHelperService, AuthenticationHelperService>();
+            services.AddSingleton<IWorkerClientService, WorkerClientService>();
+        }
+
+        public static void ConfigureLogging(HostBuilderContext context,
+            ILoggingBuilder logger)
+        {
+            logger.ClearProviders();
+            logger.AddConsole();
+            logger.SetMinimumLevel(LogLevel.Information);
         }
     }
 }
--- TTWebMVC/TTWeb.Worker.SchedulePlanningTrigger/Program.cs
+++ TTWebMVC/TTWeb.Worker.SchedulePlanningTrigger/Program.cs
@@ -1,13 +1,5 @@
-using System;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using TTWeb.BusinessLogic.Models.AppSettings;
-using TTWeb.BusinessLogic.Models.AppSettings.Authentication;
-using TTWeb.BusinessLogic.Models.AppSettings.Scheduling;
-using TTWeb.BusinessLogic.Services.Authentication;
-using TTWeb.BusinessLogic.Services.Client;
 using TTWeb.BusinessLogic.Services.Worker;
 
 namespace TTWeb.Worker.SchedulePlanningTrigger
@@ -21,29 +13,12 @@ namespace TTWeb.Worker.SchedulePlanningTrigger
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureAppConfiguration((context, config) =>
-                {
-                    config.SetBasePath(AppContext.BaseDirectory)
-                        .AddJsonFile($"appsettings.json", optional: true)
-                        .AddJsonFile($"appsettings.{context.HostingEnvironment.EnvironmentName}.json", optional: false);
-                })
+                .ConfigureAppConfiguration(WorkerClientService.ConfigureAppConfiguration)
                 .ConfigureServices((context, services) =>
                 {
-                    services.Configure<HttpClientAppSettings>(context.Configuration.GetSection(HttpClientAppSettings.Section));
-                    services.Configure<WorkerAppSettings>(context.Configuration.GetSection(WorkerAppSettings.Section));
-                    services.Configure<AuthenticationAppSettings>(context.Configuration.GetSection(AuthenticationAppSettings.Section));
-                    services.Configure<SchedulingAppSettings>(context.Configuration.GetSection(SchedulingAppSettings.Section));
-
-                    services.AddHttpClient<WorkerWebApiClient>();
-                    services.AddSingleton<IAuthenticationHelperService, AuthenticationHelperService>();
-                    services.AddSingleton<IWorkerClientService, WorkerClientService>();
+                    WorkerClientService.ConfigureServices(context, services);
                     services.AddHostedService<TriggerPlanningWorker>();
                 })
-                .ConfigureLogging(o =>
-                {
-                    o.ClearProviders();
-                    o.AddConsole();
-                    o.SetMinimumLevel(LogLevel.Information);
-                });
+                .ConfigureLogging(WorkerClientService.ConfigureLogging);
     }
 }
