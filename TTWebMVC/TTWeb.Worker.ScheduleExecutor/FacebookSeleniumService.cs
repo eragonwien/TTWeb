3c7918ed7ba6e41280ce62852cb873494ea6da26
Hoang Son Nguyen <nguyennhoangson@gmail.com>
2020-12-05 07:56:17 +0100

    adds selenium nuget

--- TTWebMVC/TTWeb.Worker.ScheduleExecutor/FacebookSeleniumService.cs
+++ TTWebMVC/TTWeb.Worker.ScheduleExecutor/FacebookSeleniumService.cs
@@ -1,13 +1,45 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using TTWeb.BusinessLogic.Models.Entities;
 
 namespace TTWeb.Worker.ScheduleExecutor
 {
     public class FacebookSeleniumService : IFacebookSeleniumService
     {
-        public Task ProcessAsync(ScheduleJobModel workingJob)
+        public async Task ProcessAsync(ScheduleJobModel workingJob)
         {
-            throw new System.NotImplementedException();
+            if (workingJob is null) throw new ArgumentNullException(nameof(workingJob));
+
+            switch (workingJob.Action)
+            {
+                case Data.Models.ScheduleAction.Like:
+                    await LikeAsync(workingJob);
+                    break;
+                case Data.Models.ScheduleAction.Comment:
+                    await CommentAsync(workingJob);
+                    break;
+                case Data.Models.ScheduleAction.Post:
+                    await PostAsync(workingJob);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private Task LikeAsync(ScheduleJobModel workingJob)
+        {
+            if (workingJob is null) throw new ArgumentNullException(nameof(workingJob));
+            throw new NotImplementedException();
+        }
+
+        private Task CommentAsync(ScheduleJobModel workingJob)
+        {
+            throw new NotImplementedException();
+        }
+
+        private Task PostAsync(ScheduleJobModel workingJob)
+        {
+            throw new NotImplementedException();
         }
     }
 }
