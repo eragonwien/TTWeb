80425707d663b81f02159d7c22d7e964460e44fc
Hoang Son Nguyen <nguyennhoangson@gmail.com>
2021-01-14 22:27:54 +0100

    adds new fields to table schedule job result

---
+++ TTWebMVC/TTWeb.Data/Migrations/20210114212725_AddsScheduleJobResultMessageFields.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TTWeb.Data.Migrations
+{
+    public partial class AddsScheduleJobResultMessageFields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ErrorMessage",
+                table: "ScheduleJobResult",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Message",
+                table: "ScheduleJobResult",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "ScheduleJobResult",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ErrorMessage",
+                table: "ScheduleJobResult");
+
+            migrationBuilder.DropColumn(
+                name: "Message",
+                table: "ScheduleJobResult");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "ScheduleJobResult");
+        }
+    }
+}
--- TTWebMVC/TTWeb.Data/Models/ScheduleJobResult.cs
+++ TTWebMVC/TTWeb.Data/Models/ScheduleJobResult.cs
@@ -2,9 +2,13 @@
 {
     public class ScheduleJobResult : IHasIdEntity
     {
-        public int ScheduleJobId { get; set; }
-        public ScheduleJob ScheduleJob { get; set; }
         public int Id { get; set; }
+        public int ScheduleJobId { get; set; }
+        public ProcessingStatus Status { get; set; }
+        public string Message { get; set; }
+        public string ErrorMessage { get; set; }
+
+        public ScheduleJob ScheduleJob { get; set; }
 
         // TODO: Adds more details, Adds mapping profile, Updates model
     }
